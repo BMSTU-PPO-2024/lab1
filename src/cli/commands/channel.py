22a1b87e68e0558257ad4087dd47b07d0fd80373
Roman Bakaldin <gbakaldin@gmail.com>
2024-05-27 02:59:46 +0300

    Implement channel commands

--- src/cli/commands/channel.py
+++ src/cli/commands/channel.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Set
 
 from client.HttpRequest import HttpMethod
 from manager.ICommand import ICommand
@@ -17,82 +17,133 @@ def register(commands: List[ICommand]):
 class Get(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'get-channel'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.GET
 
     def get_url(self) -> str:
-        pass
+        return '/channel/{id}'
+
+    def get_path_params(self) -> Set[str]:
+        return {'id'}
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
 
 
 class Find(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'find-channel'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.GET
 
     def get_url(self) -> str:
-        pass
+        return '/channel'
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
+
+    def get_queries(self) -> Set[str]:
+        return {'name', 'pattern', 'page', 'batch'}
 
 
 class ListPosts(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'list-channel-posts'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.GET
 
     def get_url(self) -> str:
-        pass
+        return '/channel/{id}/posts'
+
+    def get_path_params(self) -> Set[str]:
+        return {'id'}
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
+
+    def get_queries(self) -> Set[str]:
+        return {'page', 'batch'}
 
 
 class Put(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'put-channel'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.PUT
 
     def get_url(self) -> str:
-        pass
+        return '/channel'
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
+
+    def get_body_params(self) -> Set[str]:
+        return {'name', 'visible'}
 
 
 class PublishPost(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'publish-post'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.PUT
 
     def get_url(self) -> str:
-        pass
+        return '/channel/{id}/post'
+
+    def get_path_params(self) -> Set[str]:
+        return {'id'}
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
+
+    def get_body_params(self) -> Set[str]:
+        return {'title', 'text', 'visible', 'tagIds'}
 
 
 class Update(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'upd-channel'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.PATCH
 
     def get_url(self) -> str:
-        pass
+        return '/channel/{id}'
+
+    def get_path_params(self) -> Set[str]:
+        return {'id'}
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
+
+    def get_body_params(self) -> Set[str]:
+        return {'name', 'visible'}
 
 
 class Delete(ICommand):
 
     def get_name(self) -> str:
-        pass
+        return 'del-channel'
 
     def get_method(self) -> HttpMethod:
-        pass
+        return HttpMethod.DELETE
 
     def get_url(self) -> str:
-        pass
+        return '/channel/{id}'
+
+    def get_path_params(self) -> Set[str]:
+        return {'id'}
+
+    def get_headers(self) -> Set[str]:
+        return {'Authorization'}
